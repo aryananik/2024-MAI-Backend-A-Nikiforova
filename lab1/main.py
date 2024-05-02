d145585c7511a06c63008cf1c2e6e684305d5289
aryananik <aryananikiforova@gmail.com>
2024-05-03 00:27:01 +0300

    lab1

---
+++ lab1/main.py
@@ -0,0 +1,11 @@
+from cache import LRUCache
+
+cache = LRUCache(100)
+cache.set('Jesse', 'Pinkman')
+cache.set('Walter', 'White')
+cache.set('Jesse', 'James')
+print(cache.get('Jesse'))
+cache.rem('Walter')
+print(cache.get('Walter'))
+
+print(cache)
